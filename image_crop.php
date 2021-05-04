a97ad0eb745f7cf5cb5d060a7ee6c3687164b0e0
orderbot-tech <raiyan24r@gmail.com>
2021-05-05 00:33:45 +0600

    cropping works 10%

--- image_crop.php
+++ image_crop.php
@@ -2,34 +2,16 @@
 
 
 
+$sentImage = 'http://localhost/image_crop/ss1.jpg';
+$croppedimg = cropImage($sentImage);
 
 
+?>
+
+<img src="<?= $croppedimg; ?>" />
 
 
-
-
-
-
-
-$attachmentLink = [];
-
-
-
-
-
-
-
-
-
-
-
-
-      $croppedimg = cropImage($sentImage);
-
-
-
-
-
+<?php
 function cropImage($sentImage)
 {
 
@@ -37,26 +19,24 @@ function cropImage($sentImage)
 
   $finalImage = imagecreatefromjpeg($sentImage);
 
+  $im2 = imagecrop($finalImage, ['x' => 0, 'y' => imagesy($finalImage) * 0.1, 'width' => imagesx($finalImage), 'height' => imagesy($finalImage) * 0.9]);
 
 
-  if (imagesy($finalImage) > 2.5 * imagesx($finalImage)) {
-    $im2 = imagecrop($finalImage, ['x' => 0, 'y' => imagesy($finalImage) * 0.4, 'width' => imagesx($finalImage), 'height' => imagesy($finalImage) * 0.3]);
-  } else if (imagesy($finalImage) > 2 * imagesx($finalImage)) {
-    $im2 = imagecrop($finalImage, ['x' => 0, 'y' => imagesy($finalImage) * 0.3, 'width' => imagesx($finalImage), 'height' => imagesy($finalImage) * 0.4]);
-  } else if (imagesy($finalImage) > 1.6 * imagesx($finalImage)) {
-    $im2 = imagecrop($finalImage, ['x' => 0, 'y' => imagesy($finalImage) * 0.2, 'width' => imagesx($finalImage), 'height' => imagesy($finalImage) * 0.6]);
-  } else if (imagesy($finalImage) > 1.4 * imagesx($finalImage)) {
-    $im2 = imagecrop($finalImage, ['x' => 0, 'y' => imagesy($finalImage) * 0.15, 'width' => imagesx($finalImage), 'height' => imagesy($finalImage) * 0.7]);
-  } else if (imagesy($finalImage) > 1.2 * imagesx($finalImage)) {
-    $im2 = imagecrop($finalImage, ['x' => 0, 'y' => imagesy($finalImage) * 0.05, 'width' => imagesx($finalImage), 'height' => imagesy($finalImage) * 0.9]);
-  } else $im2 = $finalImage;
+  /*
+  if (imagesy($finalImage) > 1.5 * imagesx($finalImage)) {
+    $im2 = imagecrop($finalImage, ['x' => 0, 'y' => imagesy($finalImage) * 0.1, 'width' => imagesx($finalImage), 'height' => imagesy($finalImage) * 0.9]);
+  } 
+
+*/
 
 
-
-
-  imagepng($im2, 'converted.png');
+  $img_name = 'converted' . rand(10, 100) . '.png';
+  imagepng(
+    $im2,
+    $img_name
+  );
 
 
 
-  return 'converted.png';
+  return  $img_name;
 }
